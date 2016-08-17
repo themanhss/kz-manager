694c63f182c4871053626b548e44103b667fe441
manhnt <manhnt@qsoft.com.vn>
2016-08-17 09:50:36 +0700

    đ

--- tests/joinGroup2.php
+++ tests/joinGroup2.php
@@ -39,7 +39,7 @@ class shareToGroup extends Illuminate\Foundation\Testing\TestCase {
             // auth google
             $username = $this->webDriver->findElement(WebDriverBy::id('Email'));
             if ($username->isDisplayed()) {
-                $username->sendKeys('hoaiduc.love09@gmail.com');
+                $username->sendKeys('longbien.love09@gmail.com');
             }
 
             $this->webDriver->manage()->timeouts()->implicitlyWait(5);
